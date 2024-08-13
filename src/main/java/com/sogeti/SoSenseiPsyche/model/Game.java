535d0a46c95cc72cf09ab6d9507a56050b440a00
Chiel de Cock <chiel.de.cock@sogeti.com>
2024-08-13 12:43:29 +0200

    Added variables Updated startGame method Added TODO-comment for later updating makeGuess method Added isGameOver method (boolean)

--- src/main/java/com/sogeti/SoSenseiPsyche/model/Game.java
+++ src/main/java/com/sogeti/SoSenseiPsyche/model/Game.java
@@ -1,24 +1,33 @@
 package com.sogeti.SoSenseiPsyche.model;
 
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     public static final int CODE_LENGTH = 4;
+    public static final int MAX_ATTEMPTS = 12;
+    public int attempt = 0;
+    private Code secretCode;
+    private List<Guess> guesses;
 
 // for testing purpose
 
     public void startGame() {
-        System.out.println("Welcome to Mastermind!");
-        System.out.println("Available colors: ");
+        secretCode = new Code();
+        System.out.println("Welcome to So Sensei Psyche!");
 
-        for (Color color : Color.values()) {
-            System.out.println(color.name() + " (" + color.name().charAt(0) + ")");
+        while (!isGameOver()) {
+            makeGuess(new Scanner(System.in));
+
+            // feedback logic
+
+            attempt++;
         }
-
-        makeGuess(new Scanner(System.in));
     }
 
+    // TODO: return type should be Feedback. Adjustments need to be made when
+    //  class Feedback and logic is finished.
     public static String[] makeGuess(Scanner scanner) {
         System.out.print("Enter your guess (e.g., RGBY): ");
         String input = scanner.nextLine().toUpperCase().trim();
@@ -34,6 +43,15 @@ public class Game {
         }
         return guess;
     }
+
+    public boolean isGameOver() {
+        if (attempt == MAX_ATTEMPTS) {
+            System.out.println("GAME OVER");
+            return true;
+        }
+        return false;
+    }
+
     public static boolean isCorrectColor(char c) {
         for (Color color : Color.values()) {
             if (color.name().charAt(0) == c) {
