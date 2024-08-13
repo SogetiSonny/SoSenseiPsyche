af514f766cdc59a413bc85bf201ddd548975bb57
lheiden <leon.vander.heiden@sogeti.com>
2024-08-13 09:52:48 +0200

    Removed test logic in the main() method

--- src/main/java/com/sogeti/SoSenseiPsyche/SoSenseiPsycheApplication.java
+++ src/main/java/com/sogeti/SoSenseiPsyche/SoSenseiPsycheApplication.java
@@ -1,6 +1,5 @@
 package com.sogeti.SoSenseiPsyche;
 
-import com.sogeti.SoSenseiPsyche.model.Code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
