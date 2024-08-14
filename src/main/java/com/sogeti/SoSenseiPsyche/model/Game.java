b3d31a131c8273933ee99b2c92f24e1538255c5b
SimoneCelli80 <158849846+SimoneCelli80@users.noreply.github.com>
2024-08-14 15:59:40 +0200

    logic to compare secret code and guessed code and for returning the feedback implemented

--- src/main/java/com/sogeti/SoSenseiPsyche/model/Game.java
+++ src/main/java/com/sogeti/SoSenseiPsyche/model/Game.java
@@ -1,42 +1,61 @@
 package com.sogeti.SoSenseiPsyche.model;
 
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Game {
     public static final int CODE_LENGTH = 4;
 
 // for testing purpose
+    Feedback feedback = new Feedback();
+    Code code = new Code();
+    List<Character> secretCode = code.getCode();
 
     public void startGame() {
+        System.out.println("secretcode" + secretCode);
         System.out.println("Welcome to Mastermind!");
         System.out.println("Available colors: ");
 
         for (Color color : Color.values()) {
             System.out.println(color.name() + " (" + color.name().charAt(0) + ")");
         }
-        makeGuess();
+        System.out.println(secretCode);
+        List<Character> guessedCode = makeGuess();
+        System.out.println(feedback.getFeedback(guessedCode, secretCode));
+
     }
 
-    public static String[] makeGuess() {
+    public static List<Character> makeGuess() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your guess (e.g., RGBY): ");
         String input = scanner.nextLine().toUpperCase().trim();
+        List<Character> guessedCode = new ArrayList<>();
+
         if (input.length() != CODE_LENGTH) {
             System.out.println("Please enter 4 characters.");
             makeGuess();
         }
-        String[] guess = new String[CODE_LENGTH];
+
         for (int i = 0; i < CODE_LENGTH; i++) {
             if (!isCorrectColor(input.charAt(i))) {
                 System.out.println("Please enter valid colors");
                 makeGuess();
                 return null;
+            } else {
+                guessedCode.add(input.charAt(i));
             }
-            guess[i] = String.valueOf(input.charAt(i));
         }
-        return guess;
+
+        return guessedCode;
+
     }
+
+
     public static boolean isCorrectColor(char c) {
         for (Color color : Color.values()) {
             if (color.name().charAt(0) == c) {
@@ -45,4 +64,5 @@ public class Game {
         }
         return false;
     }
+
 }
