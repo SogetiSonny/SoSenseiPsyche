222f72873c7673c23767def6e5cdfd8a60604c09
Sonny Lo <sonny.lo@sogeti.com>
2024-08-22 10:19:26 +0200

    Added option for user to quit the game

--- src/main/java/com/sogeti/SoSenseiPsyche/model/Game.java
+++ src/main/java/com/sogeti/SoSenseiPsyche/model/Game.java
@@ -31,6 +31,7 @@ public class Game {
         while (!isGameOver()) {
             System.out.println("You have "+ attemptsRemaining + " attempts to crack the code.");
             System.out.println("You can choose from the following colors: " + ColorMapper.getColorMapping());
+            System.out.println("You can quit the game by typing 'quit'");
             userGuess();
             for (String guess : guesses) {
                 System.out.println(guess.replace("[", "").replace("]", ""));
@@ -43,6 +44,14 @@ public class Game {
     private void userGuess() {
         System.out.print("Enter your guess (e.g., RGBY): ");
         String[] guess = scanner.nextLine().toUpperCase().split(" ");
+
+        for(String guessString : guess) {
+            if (guessString.equalsIgnoreCase("quit")) {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            }
+        }
+
         GuessValidation guessValidation = new GuessValidation();
 
         while(!guessValidation.userInputIsValid(guess)) {
